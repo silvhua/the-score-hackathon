31e1c5cbb7f36640337987472b7d8fd17eb6da35
Liu Ladniak <liuladniak@gmail.com>
2024-07-04 20:23:48 -0400

    adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import EventPage from "./pages/EventPage/EventPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      The Score
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<HomePage />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import "./ProfilePage.scss";
+
+const ProfilePage = () => {
+  return <div>ProfilePage</div>;
+};
+
+export default ProfilePage;
