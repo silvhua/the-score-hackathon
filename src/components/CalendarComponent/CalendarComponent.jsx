e95fe8cf222bd562680943c6feea73b09a0b39d6
silvhua <silvhua@gmail.com>
2024-07-04 21:07:42 -0700

    Load data from `HomePage`

--- src/components/CalendarComponent/CalendarComponent.jsx
+++ src/components/CalendarComponent/CalendarComponent.jsx
@@ -1,35 +1,39 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./CalendarComponent.scss";
 import Calendar from "react-calendar";
 import isSameDate from "../../utils/utils";
-import data from '../../data/data.json';
+// import data from '../../data/data.json';
 
-const CalendarComponent = () => {
-  const datesToAddClassTo = [new Date()];
+const CalendarComponent = ({eventData, handleDateClick}) => {
   const eventClass = 'react-calendar__tile--event'
   const dateTileClass = 'react-calendar__tile'
-  const [value, setValue] = useState(new Date());
-  let eventDates = data.events.map(event => new Date(event.datetime));
+  const todaysDate = new Date();
+  // const [value, setValue] = useState(new Date());
+  let eventDates = eventData.events.map(event => new Date(event.datetime));
 
   function tileClassName({ date, view }) {
+    let classNames = '';
     // Add class to tiles in month view only
     if (view === 'month') {
       // Check if a date React-Calendar wants to check is on the list of dates to add class to
       if (eventDates.find(dDate => isSameDate(dDate, date))) {
-        return eventClass;
+        classNames = eventClass;
       } else {
-        return dateTileClass;
+        classNames = dateTileClass;
       }
     }
+    return classNames;
+
   }
 
-  function onChange(nextValue) {
-    setValue(nextValue);
-  }
+  // function onChange(nextValue) {
+  //   setValue(nextValue);
+  // }
   return (
     <>
       <Calendar className='calendar'
-        onChange={onChange}
+        // onChange={onChange}
+        onChange={handleDateClick}
         tileClassName={tileClassName}
       />
     </>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 import CalendarComponent from "../../components/CalendarComponent/CalendarComponent";
+import eventData from '../../data/data.json';
 
 import EventsList from "../../components/EventsList/EventsList.jsx";
 
 const HomePage = () => {
+
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateClick = (value) => {
+    setSelectedDate(value);
+  }
+
+  // console.log(selectedDate)
   return (
     <main>
-      <CalendarComponent />
+      <CalendarComponent 
+        eventData={eventData} 
+        handleDateClick={handleDateClick}
+        />
       <EventsList />
     </main>
   
