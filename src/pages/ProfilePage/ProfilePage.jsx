43de8cfdd9f4847f38211536fc0f7f29f2849235
Irene Tsang <tsangchiyan@irenes-mbp.home>
2024-07-04 23:38:03 -0400

    Added Header to Profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,12 @@
+import Header from "../../components/Header/Header";
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+  <div>
+        <Header pageTitle={"Profile"} />
+  </div>
+  )
 };
 
 export default ProfilePage;
