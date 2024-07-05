1511021f5b124e69399ed86cb511b715f87ef58a
silvhua <silvhua@gmail.com>
2024-07-04 19:23:05 -0700

    import events so calendar has styling for dates with events

--- src/components/CalendarComponent/CalendarComponent.jsx
+++ src/components/CalendarComponent/CalendarComponent.jsx
@@ -8,21 +8,20 @@ const CalendarComponent = () => {
   const datesToAddClassTo = [new Date()];
   const className = 'date__today'
   const [value, setValue] = useState(new Date());
+  let eventDates = data.events.map(event => new Date(event.datetime));
+  console.log(eventDates);
 
   function tileClassName({ date, view }) {
     // Add class to tiles in month view only
     if (view === 'month') {
       // Check if a date React-Calendar wants to check is on the list of dates to add class to
-      if (datesToAddClassTo.find(dDate => isSameDate(dDate, date))) {
+      if (eventDates.find(dDate => isSameDate(dDate, date))) {
         return className;
       }
     }
   }
-  console.log(data);
 
   function onChange(nextValue) {
-    console.log('next value', nextValue.setHours(0, 0,0,0));
-    console.log('today value', datesToAddClassTo[0]);
     setValue(nextValue);
   }
   return (
