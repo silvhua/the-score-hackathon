46ea344a47a18536bc202df0120ab5d55da01988
silvhua <silvhua@gmail.com>
2024-07-05 09:30:13 -0700

    Fix header to top of page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,14 +11,14 @@ const Header = ({pageTitle}) => {
 
 
   return (
-    <div className="header">
+    <header className="header">
       <Link to={"/"} className="header__logo"><img src={logo} alt={"the Score Logo"}/></Link>
     <div className="header__title">
         <img src={backArrow} alt="Back Button" onClick={() => navigate(-1)} className="header__button-back"/>
         <h1 className="header__title-text">{pageTitle}</h1>
         <Link to={"/profile"}><img src={profileIcon} alt={"Profile Icon"}/></Link>
     </div>
-    </div>
+    </header>
   )
 }
 
