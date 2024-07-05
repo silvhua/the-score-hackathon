8fa09ef546a956e98320149d54309379fa0d8e2c
Liu Ladniak <liuladniak@gmail.com>
2024-07-05 10:45:47 -0400

    profile initial setup

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,29 @@
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <section className="profile">
+      <div className="heading">Header</div>
+
+      <div className="events">
+        <div className="events-upcoming">
+          <h3>Upcoming events</h3>
+          <div>Upcoming event 1</div>
+          <div>Upcoming event 1</div>
+        </div>
+
+        <div className="events-joined">
+          <h3>You Joined Communities</h3>
+          <div className="community-card">
+            <h4 className="community-card__title">Community Name</h4>
+            <span className="community-card__people">5 people</span>
+          </div>
+
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProfilePage;
