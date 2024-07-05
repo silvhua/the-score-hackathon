f1822c2442e802dec60e42547d019c334f419309
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-07-05 00:41:49 -0400

    format cards with event details

--- src/components/EventsList/EventsList.jsx
+++ src/components/EventsList/EventsList.jsx
@@ -4,7 +4,15 @@ import eventsData from "../../data/data.json";
 
 // this component will take in filtered data
 const EventsList = () => {
-  //   console.log(eventsData.events.length);
+  //   const formatter = new Intl.DateTimeFormat("en-US", {
+  //     day: "2-digit",
+  //     month: "2-digit",
+  //     year: "numeric",
+  //   });
+
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    hour: "2-digit",
+  });
 
   return (
     <main>
@@ -13,7 +21,32 @@ const EventsList = () => {
           // console.log(event);
           <section className="containers" key={event.id}>
             <Link to={`/event/${event.id}`} className="event-card__container">
-              <section className="event-card">{event.name}</section>
+              <section className="event-card">
+                {/* <div className="text"> */}
+                <div className="event__title">
+                  {event.name}
+                  {/* <div>{event.name === "Baller Meetup" || event.name === "Hoop Heroes Hangout" ? ${}}</div> */}
+                </div>
+                <div className="event__details">
+                  <div className="event__details--col-1">
+                    <div className="time">
+                      {formatter.format(Date.parse(event.datetime))}
+                    </div>
+                    <div className="cost">
+                      {event.id % 2 === 0 ? "$$" : "FREE"}
+                    </div>
+                  </div>
+                  <div className="event__details--col-2">
+                    <div className="location">{event.location}</div>
+                    <div className="attendees">
+                      {" "}
+                      {event.attendees.length} attendees
+                    </div>
+                  </div>
+                </div>
+                {/* </div> */}
+                {/* <div className="icon"></div> */}
+              </section>
             </Link>
           </section>
         );
