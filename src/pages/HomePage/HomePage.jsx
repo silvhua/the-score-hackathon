caf675f6c738c95302ea407a8eddb595085c5c3e
silvhua <silvhua@gmail.com>
2024-07-05 02:16:40 -0700

    If selected date does not have events, render the next 5 events

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,40 +2,54 @@ import { useEffect, useState } from "react";
 import "./HomePage.scss";
 import CalendarComponent from "../../components/CalendarComponent/CalendarComponent";
 import data from '../../data/data.json';
-import isSameDate from "../../utils/utils.js";
-
+import isSameDate, {formatDate} from "../../utils/utils.js";
 
 import EventsList from "../../components/EventsList/EventsList.jsx";
 
 const HomePage = () => {
   let eventsData = data.events;
   const [selectedDate, setSelectedDate] = useState(null);
-  const [filteredEvents, setFilteredEvents] = useState(eventsData)
+  const [filteredEvents, setFilteredEvents] = useState(null);
+  const [subtitle, setSubtitle] = useState();
+  let eventDates = eventsData.map(event => new Date(event.datetime));
 
   const handleDateClick = (value) => {
     setSelectedDate(value);
   }
 
   useEffect(() => {
-    if (selectedDate) {
+    if (selectedDate && eventDates.find(eventDate => isSameDate(selectedDate, eventDate))) {
       const filteredData = eventsData.filter((object) => {
         const eventDate = new Date(object.datetime);
         return isSameDate(eventDate, selectedDate);
       })
-      setFilteredEvents(filteredData)
-
+      setFilteredEvents(filteredData);
+      setSubtitle(`Events for ${formatDate(selectedDate)}`);
+    } else {
+      setFilteredEvents(eventsData.slice(0,3));
+      setSubtitle('Upcoming Events');
     }
   }, [selectedDate])
   return (
-    <main className='home'>
-      <CalendarComponent 
-        eventsData={eventsData} 
-        handleDateClick={handleDateClick}
-        selectedDate={selectedDate}
-      />
-      <EventsList 
-        eventsData={filteredEvents} 
-      />
+    <main>
+      <div  className='home'>
+        <h1 className='home__title'>Welcome to the Score's {data.location} Community</h1>
+        <div className='home__body'>
+          <CalendarComponent 
+            eventsData={eventsData} 
+            handleDateClick={handleDateClick}
+            selectedDate={selectedDate}
+            eventDates={eventDates}
+          />
+          <EventsList 
+            eventsData={filteredEvents} 
+            selectedDate={selectedDate}
+            subtitle={subtitle}
+          />
+          
+        </div>
+
+      </div>
     </main>
   
   );
