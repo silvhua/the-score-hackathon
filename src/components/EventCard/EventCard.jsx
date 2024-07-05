c972c140a8b530cb9337236fecf289d405792e0f
Liu Ladniak <liuladniak@gmail.com>
2024-07-05 14:29:57 -0400

    adds icons

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,34 +1,50 @@
 import "./EventCard.scss";
 import { formatDate } from "../../utils/utils";
-// import SportIcon from "../SportIcon/SportIcon";
+import dateIcon from "../../assets/icon/date-white.svg";
+import groupIcon from "../../assets/icon/group-white.svg";
+import locationIcon from "../../assets/icon/location-white.svg";
+import timeIcon from "../../assets/icon/time-white.svg";
 
 const formatter = new Intl.DateTimeFormat("en-US", {
   hour: "2-digit",
 });
 
-const EventCard = ({ name, datetime, location, cost, attendees, icons }) => {
+const EventCard = ({ name, datetime, location, cost, attendees }) => {
   return (
     <div className="profile-event-card">
       <div className="profile-event-card__content">
         <h3 className="profile-event-card__name">{name}</h3>
-        <span className="profile-event-card__date">
-          {formatDate(new Date(datetime))}
-        </span>
-        <div className="profile-event-card__details">
-          <span className="profile-event-card__time">
-            {formatter.format(Date.parse(datetime))}
+        <div className="icon-wrp">
+          <img src={dateIcon} alt="date icon" />
+          <span className="profile-event-card__date">
+            {formatDate(new Date(datetime))}
           </span>
-          <span className="profile-event-card__location">{location}</span>
         </div>
         <div className="profile-event-card__details">
-          <span className="profile-event-card__cost">{cost}</span>
-          <span className="profile-event-card__attendees">
-            {attendees} attendees
-          </span>
+          <div className="icon-wrp">
+            <img src={timeIcon} alt="time icon" />
+            <span className="profile-event-card__time">
+              {formatter.format(Date.parse(datetime))}
+            </span>
+          </div>
+          <div className="icon-wrp">
+            <img src={locationIcon} alt="location icon" />
+            <span className="profile-event-card__location">{location}</span>
+          </div>
+        </div>
+        <div className="profile-event-card__details">
+          <div className="icon-wrp">
+            <img className="icon-price" src={groupIcon} alt="location icon" />
+            <span className="profile-event-card__cost">{cost}</span>
+          </div>
+          <div className="icon-wrp">
+            <img src={groupIcon} alt="location icon" />
+            <span className="profile-event-card__attendees">
+              {attendees} attendees
+            </span>
+          </div>
         </div>
       </div>
-
-      {/* <SportIcon eventName={name} icons={icons} /> */}
     </div>
   );
 };
